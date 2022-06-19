01d11e82a5b84f64ece7dc0fd6edfebad184edaa
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-19 23:42:17 +0500

    macro

--- robot/src/App.js
+++ robot/src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 import { robots } from "./robots";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import "./App.css"
+import "./App.css";
 class App extends React.Component {
   state = {
-    robots: robots,
+    robots: [],
     searchfield: "",
   };
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
   onSearchChange = (event) => {
     this.setState({
       searchfield: event.target.value,
