2472d12d19d011cb403958ec2716b5cb975c324b
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-19 23:25:45 +0500

    dd

---
+++ robot/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { robots } from "./robots";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import "./App.css"
+class App extends React.Component {
+  state = {
+    robots: robots,
+    searchfield: "",
+  };
+  onSearchChange = (event) => {
+    this.setState({
+      searchfield: event.target.value,
+    });
+  };
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLocaleLowerCase());
+    });
+
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
+
+export default App;
