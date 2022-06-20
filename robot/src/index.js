247e59b644e7afbbfcb69cd75e98bdb19cd85b46
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-20 21:32:17 +0500

    changed container folder

--- robot/src/index.js
+++ robot/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-import App from "./components/App";
+import App from "./container/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
